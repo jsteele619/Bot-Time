bf10b173b7a76016fb7d22479796e21bfa8ce8a2
jeremysteele <jeremysteelewriter@gmail.com>
2021-06-17 21:23:25 -0700

    3.0 mvp

--- bot.py
+++ bot.py
@@ -4,22 +4,24 @@ import discord
 import random
 
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    print(f'{guild.name}')
+    print(f'{client.guilds}')
 
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return 
 
     if message.content == "python":
         response = "random quote"
-        await message.channel.send(response)
+        await message.channel.send('Hello World!')
 
 client.run(TOKEN)
